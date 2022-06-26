a86b251cad1787e5ee8c50b6e852bfe7d16e3742
semihorhan <sorhan3.14@gmail.com>
2022-06-26 20:12:14 +0300

    add render function

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
-let ulEl = document.getElementById("ul-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    // Clear out the input field
+    renderLeads()
 })
 
-// Render the leads in the unordered list using ulEl.textContent
-for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i])
-    ulEl.textContent += myLeads[i]
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems  
 }
-
-
-
